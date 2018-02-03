8388f931e424cc9fdd7e69837d6f4cabecb8347a
Svenmeister <sven@blauweman.nl>
2018-02-03 19:24:33 +0100

    Added coinflip!

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,8 +1,8 @@
 module.exports = {
      main: function(bot, msg) {
        let flip = Math.floor(Math.random() * 2) + 1;
-       if(flip == 1) let flipw = 'Head!';
-       if(flip == 2) let flipw = 'Tail!';
+       if(flip == 1) var flipw = 'Head!';
+       if(flip == 2) var flipw = 'Tail!';
        bot.sendNotification(flipw, "info", msg);
     },
     help: 'Flips a virtual coin.'
