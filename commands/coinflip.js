8d55eddd1e806a6210d5bd828198cec965f7c56c
Svenmeister <sven@blauweman.nl>
2018-02-03 19:31:50 +0100

    Adding <:coin:388422093227950090> to the coinflip results!

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,9 +1,9 @@
 module.exports = {
-     main: function(bot, msg) {
+     main: function(bot, msg, emoji) {
        let flip = Math.floor(Math.random() * 2) + 1;
        if(flip == 1) var flipw = 'Head!';
        if(flip == 2) var flipw = 'Tail!';
-       bot.sendNotification(flipw, "info", msg);
+       bot.sendNotification(flipw + ' <:coin:388422093227950090> ', "info", msg);
     },
     help: 'Flips a virtual coin.'
 };
